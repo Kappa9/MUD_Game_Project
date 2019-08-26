040cbef8063ae5cb50ad6a6e237c487510e0081a
whiter0428 <1784326368@qq.com>
2019-08-26 09:56:06 +0800

    关于Goods物品的更改

--- MUD_Game_Project/GameData.h
+++ MUD_Game_Project/GameData.h
@@ -1,3 +1,26 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Goods {
+public:
+	char Nume[32];          //名称
+	char Description[512];  //描述
+	int Attribute;          //属性    0-可消耗，1-装备，2-学习
+	int AddAtract;          //增加攻击
+	int AddDefense;         //增加防御
+	int AddHP;              //回复血量
+	int AddMaxHP;           //增加血量上限
+	int AddMP;              //回复蓝量
+	int AddMaxMP;           //增加蓝量上限
+	int AddSpeed;           //增加速度
+	int SkillIndex;         //技能书编号
+	int Location;           //装备位置  0-头，1-身，2-手
+	int Cost;               //价值
+};
+
+
