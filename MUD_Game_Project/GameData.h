b27aed15e2c080dbcb7a5296420f230bb137f9e1
jzh5354 <1015932592@qq.com>
2019-08-26 10:14:48 +0800

    技能

--- MUD_Game_Project/GameData.h
+++ MUD_Game_Project/GameData.h
@@ -1,3 +1,17 @@
 #pragma once
 #include<iostream>
 #include<string>
+
+using namespace std;
+
+
+class CSkill {
+public:
+	string nName[50]; //名称
+	string nDescription[256];//技能描述	
+	short nPower;//技能威力
+	short nMpCost;//消耗MP值
+	string nDamage;//伤害
+
+};
+
